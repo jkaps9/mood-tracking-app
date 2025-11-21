bd7fec20dd5e41b326b7157a8fdf3b4c3aeec770
jkaps9 <jkaps9@users.noreply.github.com>
2025-11-21 16:29:42 +0000

    deploy: 58e584f418dd55cb56627f36ee66088e9cd1533d

--- scripts/blog.js
+++
@@ -1,5 +0,0 @@
-import DecapCmsApp from "decap-cms-app";
-// Initialize the CMS object
-DecapCmsApp.init();
-// Now the registry is available via the DecapCmsApp object.
-DecapCmsApp.registerPreviewTemplate("my-template", MyTemplate);
